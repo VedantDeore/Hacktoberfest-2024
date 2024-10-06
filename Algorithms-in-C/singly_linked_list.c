7c429cb2d5b01b69eea6f7aefdd9327cc071fb7f
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2024-10-07 01:37:05 +0530

    Create  singly_linked_list.c

---
+++ Algorithms-in-C/singly_linked_list.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the beginning
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to insert a node at the end
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Function to delete a node by value
+void deleteNode(struct Node** head, int key) {
+    struct Node* temp = *head;
+    struct Node* prev = NULL;
+
+    // If head node holds the key to be deleted
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next; // Changed head
+        free(temp);         // Free old head
+        return;
+    }
+
+    // Search for the key to be deleted, keeping track of the previous node
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If the key was not present in the list
+    if (temp == NULL) {
+        printf("Node with value %d not found.\n", key);
+        return;
+    }
+
+    // Unlink the node from the linked list
+    prev->next = temp->next;
+    free(temp); // Free the memory of the node
+}
+
+// Function to display the linked list
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtBeginning(&head, 5);
+
+    printf("Linked List: ");
+    displayList(head);
+
+    printf("Deleting node with value 20\n");
+    deleteNode(&head, 20);
+    displayList(head);
+
+    return 0;
+}
